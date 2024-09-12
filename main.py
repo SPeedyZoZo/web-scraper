ca83311ecf10d967a878cfeb1a1867f62014b70d
SPeedyZoZo <zaidchu@gmail.com>
2024-09-12 14:27:18 +0300

    added parse.py to parse prompt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
-from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    clean_body_content,
+    extract_body_content)
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
@@ -16,3 +20,12 @@ if st.button("Scrape Site"):
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", cleaned_content, height=300)
     
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            
